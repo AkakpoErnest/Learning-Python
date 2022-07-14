e732d6799a0c6ea9c034d7468c472e4772763b98
develNerd <isaacakakpo4@gmail.com>
2022-07-14 16:59:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+name = input("what is your name?")
+age = input ( "what is your age?")
+int(age)
+maths = 2022 - int(age)
+int(maths)
+printe="Hello " + name + " you were born in "
+def getyear(A_ges):
+      print(printe, + A_ges)
+
+
+
+getyear(maths)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
